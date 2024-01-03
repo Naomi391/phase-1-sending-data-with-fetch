92f94832da0497fb235f64b46b67924f88c26e1c
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-01-03 15:37:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  const configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configObject)
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the response data here
+      console.log(data); // Example: Log the data to the console
+    })
+    .catch(function (error) {
+      alert("Unauthorized Access");
+      console.log(error.message);
+    });
+}
+submitData("Naomi", "nash@gmail.com");
